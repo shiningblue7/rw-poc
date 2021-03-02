1ff0659079d43ee144b65589c4f85747cc393958
Jace Benson <jace.benson@gmail.com>
2021-03-01 23:10:50 -0600

    got rules working

--- api/src/lib/rules/insert.js
+++ api/src/lib/rules/insert.js
@@ -1,65 +1,73 @@
-var fs = require('fs');
-const fsPromises = fs.promises;
-var path = require('path');
-async function listDir(dirPath){
-  try{
-    return await fsPromises.readdir(dirPath);
-  }catch(e){
-    console.error(`Error occured reading ${dirPath}`)
-  }
-}
-
-module.exports = {
-  runRulesSimple: function (current, table){
-    current.title = "simple";
-    current.id = -1;
-    return current;
-  },
-  runRules: function (current, table) {
-    console.log('getrules start')
+import * as path from 'path';
+import * as fs from 'fs/promises';
+let getRules = async function(current, table){
+  console.log(`getrules start current: ${JSON.stringify(current)}`);
+    var rules = []
     try {
-      var directory = path.resolve(`./src/lib/rules/${table}/`);
+      var directory = path.resolve(`./src/services/${table}/rules/`);
       console.log(directory);
-      var rules = [];
-      listDir(directory, (error, files) => {
-        if (error) {
-          console.error(error);
-        }
-        async ((current)=>{
-          files.forEach((file, index) => {
-            let scriptPath = path.resolve(`${directory}/${file}`);
-            console.log(`scriptPath: ${scriptPath}`)
-            let script = require(scriptPath)
-            console.log(`script: ${JSON.stringify(script)}`);
-            rules.push({
-              name: file + '',
-              command: script.command || function (current) { console.log('no script defined'); return current },
-              order: script.order || 1000000, // default 1000000
-              active: script.active || false  // default false
-            });
-            current.title = file;
-            console.log(`${index + 1}/${files.length}`)
-          });
-        })(current)
-      });
-      console.log(`rules: ${JSON.stringify(rules, '', ' ')}`)
-
+      const files = await fs.readdir(directory);
+      for await (const file of files){
+        console.log(`file ${file}`);
+        rules.push(path.resolve(`${directory}/${file}`))
+      }
+      console.log(`getrules after loop: ${JSON.stringify(current)}`);
+      return rules;
     } catch (e) {
       console.log('test.js error', e);
     }
     // sort the rules
     // filter out the inactive
-    console.log(`getrules end ${JSON.stringify(current)}`)
-    current.title = "modified by insert.js"
-    return current;
-  },
-  runRules2: function (current, table) {
+    console.log('getrules end')
+    return rules;
+}
 
-    console.log('getrules start')
+module.exports = {
+  runRules: async function (current, table){
+    let rules = [];
+    const rulePaths = await getRules(current, table);
+    console.log(`rulePaths ${rulePaths}`)
+    for await (const file of rulePaths){// rulePaths.forEach((file, index) => {
+      let scriptPath = path.resolve(`${file}`);
+      console.log(`scriptPath: ${scriptPath}`)
+      let script = require(scriptPath)
+      console.log(`script: ${JSON.stringify(script)}`);
+      rules.push({
+        name: file + '',
+        command: script.command || function (current) { console.log('no script defined'); return current },
+        order: script.order || 1000000, // default 1000000
+        active: script.active || false  // default false
+      });
+    };
+    rules.forEach(rule => {
+      console.log(`rule: ${JSON.stringify(rule, '', ' ')}`)
+      current = rule.command(current);
+    });
+    return current
+  },
+  getRules: function(current, table) {
+    console.log(`getrules start current: ${JSON.stringify(current)}`);
+    var rules = []
     try {
-      var directory = path.resolve(`./src/lib/rules/${table}/`);
+      var directory = path.resolve(`./src/services/${table}/rules/`);
       console.log(directory);
-      var rules = [];
+      const files = fs.readdir(directory);
+      for (const file of files){
+        console.log(`file ${file}`);
+        rules.push(path.resolve(`${directory}/${file}`))
+      }
+      console.log(`getrules after loop: ${JSON.stringify(current)}`);
+      return rules;
+    } catch (e) {
+      console.log('test.js error', e);
+    }
+    // sort the rules
+    // filter out the inactive
+    console.log('getrules end')
+  },
+
+      //console.log(`files ${JSON.stringify(files)}`);
+      /*
       fs.readdir(directory, (error, files) => {
         if (error) {
           console.error(error);
@@ -77,7 +85,7 @@ module.exports = {
           });
           console.log(`${index + 1}/${files.length}`)
           if ((index + 1) === files.length) {
-              console.log(`in runrules ${JSON.stringify(rules, '', ' ')}`)
+              //console.log(`in runrules ${JSON.stringify(rules, '', ' ')}`)
               rules.forEach(rule => {
                 console.log(`rule: ${JSON.stringify(rule, '', ' ')}`)
                 current = rule.command(current);
@@ -85,16 +93,11 @@ module.exports = {
               return current
           }
         });
-      });
-      console.log(`rules: ${JSON.stringify(rules, '', ' ')}`)
-    } catch (e) {
-      console.log('test.js error', e);
-    }
-    // sort the rules
-    // filter out the inactive
-    console.log('getrules end')
-  },
 
+      });
+      */
+
+      //console.log(`rules: ${JSON.stringify(rules, '', ' ')}`)
     /*
     // sort the rules
     // filter out the inactive
