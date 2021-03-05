4378a3c8ca575ab6b433a319416d751aa2090347
Jace Benson <jace.benson@gmail.com>
2021-03-04 23:34:31 -0600

    exampled updated to include date stuffs

--- api/src/rules/tickets/example-rule-loaded.js
+++ api/src/rules/tickets/example-rule-loaded.js
@@ -1,16 +1,19 @@
+import * as util from 'src/lib/util'
+
 module.exports = {
   command: function (current) {
-      try {
-        console.log(`pre-current: ${JSON.stringify(current)}`)
-          if (current.title==="jace") {
-              current.title='Jace';
-             //current.date = new Date()
-          }
-      } catch (e) {
-          console.log('test.js error', e);
+    try {
+      if (current.userId === 2) {
+        current.title += ' - Jace ';
+        var d= new Date();
+
+        current.title += `${d.toLocaleDateString()} ${d.toLocaleTimeString()} `
+        current.title += Intl.DateTimeFormat().resolvedOptions().timeZone
       }
-      console.log(`post-current: ${JSON.stringify(current)}`)
-      return current;
+    } catch (e) {
+      util.log(e);
+    }
+    return current;
   },
   active: true,
   order: 10,
