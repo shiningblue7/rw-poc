8985598f6fe82b6a1216e3263317b62900097a31
Jace Benson <jace.benson@gmail.com>
2021-04-22 23:47:19 -0500

    update about page

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -3,8 +3,9 @@ import StandardLayout from 'src/layouts/StandardLayout'
 const AboutPage = () => {
   return (
     <>
-      This site was created to demonstrate my mastery of Redwood: Look on my
-      works, ye mighty, and despair!
+      <h1>About Tskr</h1>
+      <p>This is a tool with a goal, open source, low cost, configurable task and asset tracking.  There's lots of tools out there that do this but they are either hosted by some corp, expensive, or bad.  We wanted to make a tool that you could host on a CDN, or selfhost, that has a relatively low cost, that uses new tech.</p>
+      <p>Do you like this?  Do you want support?  Reach out to me via email <a href="mailto:jace@benson.run?subject=I like what you're doing on tskr.io&body=What can I do for you?">📧</a></p>
     </>
   )
 }
